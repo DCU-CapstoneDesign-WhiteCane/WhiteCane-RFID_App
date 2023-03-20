149bda6d5432835aaf2796af5652e8b052688357
ChoSeunghyeon <jodu334@gmail.com>
2023-03-20 16:27:45 +0900

    [FEAT]Tag Page

--- src/function.js
+++ src/function.js
@@ -12,7 +12,7 @@ function consoleText(words, id, colors) {
   target.setAttribute('style', 'color:' + colors[0])
   window.setInterval(function() {
 
-    if (letterCount === 0 && waiting === false) {
+    if (letterCount === 0 && waiting === false) {      //다음 단어로 넘어갈때의 조건 ex)whitecane -> RFID
       waiting = true;
       target.innerHTML = words[0].substring(0, letterCount)
       window.setTimeout(function() {
@@ -24,19 +24,20 @@ function consoleText(words, id, colors) {
         target.setAttribute('style', 'color:' + colors[0])
         letterCount += x;
         waiting = false;
-      }, 1000)
-    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      }, 500)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {   //다음 단어가 지워지는 단계
       waiting = true;
       window.setTimeout(function() {
         x = -1;
         letterCount += x;
         waiting = false;
-      }, 1000)
+      }, 500)
     } else if (waiting === false) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
   }, 120)
+
   window.setInterval(function() {
     if (visible === true) {
       con.className = 'console-underscore hidden'
