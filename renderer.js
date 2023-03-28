f67eaea928756ec7ea1393d964f37bf70990431f
ChoSeunghyeon <jodu334@gmail.com>
2023-03-28 18:00:00 +0900

    [FEAT]fixPortlist

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-
+/*
 const { SerialPort } = require('serialport')
 const tableify = require('tableify')
 
@@ -14,6 +14,7 @@ async function listSerialPorts() {
       document.getElementById('error').textContent = ''
     }
     console.log('ports', ports);
+    console.log(SerialPort);
 
     if (ports.length === 0) {
       document.getElementById('error').textContent = 'No ports discovered'
@@ -31,6 +32,36 @@ function listPorts() {
 
 // Set a timeout that will check for new serialPorts every 2 seconds.
 // This timeout reschedules itself.
+setTimeout(listPorts, 2000);
+
+listSerialPorts()
+*/
+//조승현
+const { SerialPort } = require('serialport')
+
+async function listSerialPorts() {
+  if (ports.length == 0 ) {
+    document.getElementById("port-list").innerHTML=`
+    <ul>
+      ${ports.map(port => `<li>연결된 장치가 없습니다.</li>`).join('')}
+    </ul>
+    `
+  }
+  SerialPort.list().then(ports => {
+    document.getElementById("port-list").innerHTML=`
+    <ul>
+      ${ports.map(port => `<li>${port.friendlyName}</li>`).join('')}
+    </ul>
+    `
+  })
+  //console.log('ports', ports);
+}
+
+function listPorts() {
+  listSerialPorts();
+  setTimeout(listPorts, 2000);
+}
+
 setTimeout(listPorts, 2000);
 
 listSerialPorts()
