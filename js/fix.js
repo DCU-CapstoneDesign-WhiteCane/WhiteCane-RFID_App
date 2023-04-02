1a0f27f9176cc59b5498a116440a306d0351f508
ChoSeunghyeon <jodu334@gmail.com>
2023-04-02 18:00:25 +0900

    [FEAT]printTagLocation

---
+++ js/fix.js
@@ -0,0 +1,4 @@
+function clickFn() {
+    var pathNum = document.getElementById("list");
+    console.log(pathNum.options[pathNum.selectedIndex].value);
+  };
--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ const { SerialPort } = require('serialport')
 
 async function listSerialPorts() {
 
+  //포트리스트 출력
   await SerialPort.list().then((ports, err) => {
     if(err) {
       document.getElementById('error').textContent = err.message
@@ -10,24 +11,62 @@ async function listSerialPorts() {
       document.getElementById('error').textContent = ''
     }
     console.log('ports', ports);
-    
+
     if (ports.length === 0) {
       document.getElementById('error').textContent = '연결된 장치가 없습니다.'
     }
 
-    document.getElementById("port-list").innerHTML=`
+    document.getElementById("list").innerHTML=`
     <ul>
-      ${ports.map(port => `<option value=${port.friendlyName}>${port.friendlyName}</option>`).join('')}
+      ${ports.map(port => `<option value=${port.path}>${port.friendlyName}</option>`).join('')}
     </ul>
-    `
+    `  
   })
+
+const { ReadlineParser } = require('@serialport/parser-readline')
+var pathNum = document.getElementById("list");
+
+const port = new SerialPort(
+{ 
+    path: pathNum.options[pathNum.selectedIndex].value,
+    baudRate: 9600, 
+    autoOpen: false,
+});
+
+console.log(pathNum.options[pathNum.selectedIndex].value);
+
+const parser = port.pipe(new ReadlineParser({ delimiter: "#" }));
+
+// 시리얼 포트 오픈
+port.open((err) => {
+    if (err) {
+        return console.log("Error opening port ===> ", err.message);
+    }
+    console.log("SerialPort Open");
+});
+  
+// 시리얼 포트로부터 데이터 수신
+parser.on("data", (data) => {
+  console.log("Receive Data ===> ", data);
+  jsonParser(data);
+});
+
+// JSON 파싱 후 위치 데이터 추출
+const jsonParser = (json) => {
+  let location;
+  json = JSON.parse(json);
+
+  try {
+      if (json.hasOwnProperty("location") == true) {
+          location = json.location;
+          console.log("장소 : ", location);
+          document.getElementById("location").innerHTML=`<input placeholder="${location}"></input><button>수정하기</button>`  
+      }
+  } catch (err) {
+      console.log("error", err.message);
+  }
+}; 
+
 }
+listSerialPorts() //바로 실행하는 함수 하나
 
-function listPorts() {
-  listSerialPorts();
-  setTimeout(listPorts, 2000);
-}
-
-setTimeout(listPorts, 2000);
-
-listSerialPorts()
